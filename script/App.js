b68815b5fe59c331c4d41bd01957167cadc27908
Maryam <maryamharoon82002@gmail.com>
2023-04-06 11:57:49 -0700

    Initial comit

---
+++ script/App.js
@@ -0,0 +1,57 @@
+// Date:06-April-23
+// alert("Thanks for your input!");
+// var nationality;
+// nationality = "U.S.";
+// alert(nationality)
+// var floogle = "Mark";
+// alert(floogle)
+// var lastName = "Smith";
+// var cityOfOrigin = "New Orleans";
+// var aussieGreeting = "g'Day";
+// alert(lastName+" " +cityOfOrigin)
+//  var originalNum = 23;
+//  var numToBeAdded = 7;
+//  var newNum = originalNum + numToBeAdded;
+//  alert(newNum)
+// var originalNum = 90;
+//  originalNum = originalNum + 10;
+// alert(originalNum)
+// var caseQty = 144;
+// alert(caseQty);
+// var popularNumber = 2 + 2;
+// alert(popularNumber)
+// var num = 10;
+//  var anotherNum = 1;
+//  var popularNumber = num + anotherNum;
+//  alert(popularNumber)
+// var whatsLeftOver = 10 % 3;
+// alert(whatsLeftOver)
+// var num = 1;
+//  var newNum = num++;
+//  alert(newNum)
+// var num = 1;
+//  var newNum = num--;
+//  alert(newNum)
+// var num = 1;
+//  var newNum = --num;
+//  alert(newNum)
+// var totalCost = (1 + 3) * 4;
+// alert(totalCost)
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// alert(resultOfComputation)
+// var message = "Thanks, ";
+//  var userName = "Susan";
+//  var banger = "!";
+//  var customMess = message + userName + banger;
+//  alert(customMess)
+// var question = "Your species?";
+// alert(question)
+//  var defaultAnswer = "human";
+//  var spec = prompt(question, defaultAnswer);
+//  alert(spec)
+//  var numberOfCats = +prompt("How many cats?");
+//  alert(numberOfCats)
+//  var tooManyCats = numberOfCats + 1;
+// alert(tooManyCats)
+
+
